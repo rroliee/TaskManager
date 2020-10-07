23062b8109f968aaed155cc0e61d19c176ec7e8d
Romváry Roland <romvary.roland@onyf.hu>
2020-10-07 13:20:44 +0200

    TaskStatus can now tell possible status changes by old status

--- src/main/java/hu/kincstar/taskmanager/TaskStatus.java
+++ src/main/java/hu/kincstar/taskmanager/TaskStatus.java
@@ -1,5 +1,21 @@
 package hu.kincstar.taskmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum TaskStatus {
-    NEW, IN_PROGRESS, BLOCKED, DONE
+    NEW, IN_PROGRESS, BLOCKED, DONE;
+
+    public static List<TaskStatus> getPossibleStatusFrom(TaskStatus oldStatus){
+        switch(oldStatus){
+            case NEW:
+                return new ArrayList<>(){{add(IN_PROGRESS);add(BLOCKED);}};
+            case BLOCKED:
+                return new ArrayList<>(){{add(IN_PROGRESS);}};
+            case IN_PROGRESS:
+                return new ArrayList<>(){{add(DONE);add(BLOCKED);}};
+            default:
+                return new ArrayList<>();
+        }
+    }
 }
